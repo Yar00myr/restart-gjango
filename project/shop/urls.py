c0ed9337af5994d49aed1f0b0a8b1768ae9430cf
Yar00myr <ivanucaromir@gmail.com>
2025-03-25 22:28:49 +0000

    work on lesson but didn't finish

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, about
+from .views import home, about, product_details
 
 app_name = "shop"
 
@@ -7,4 +7,5 @@ app_name = "shop"
 urlpatterns = [
     path("home/", home, name="home"),
     path("about/", about, name="about"),
+    path("product/<int:product_id>/", product_details, name="product_details"),
 ]
--- project/shop/views.py
+++ project/shop/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 
 def home(request):
     products = Product.objects.all()
-    return render(request, "shop/index.html", {"products": products})
+    categories = Category.objects.all()
+    category_name = request.GET.get("category")
+    filter_name = request.GET.get("filter")
+
+    if category_name:
+        product = products.filter(category_name=category_name)
+
+    if filter_name == "price_increase":
+        product = product.order_by("price")
+    elif filter_name == "price_decrease":
+        product = product.order_by("-price")
+    elif filter == "increase_rating":
+        products = products.order_by("rating")
+    else:
+        products = products.order_by("-rating")
+    return render(
+        request, "shop/index.html", {"products": products, "categories": categories}
+    )
 
 
 def about(request):
     return render(request, "shop/about.html")
+
+
+def product_details(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, "shop/product_details.html", {"products": product})
