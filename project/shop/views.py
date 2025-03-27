b146b77e6fe02b15fc69164ee98a4179c8b6dd3c
Yar00myr <ivanucaromir@gmail.com>
2025-03-27 18:59:29 +0000

    filters, fix html ...

--- project/shop/views.py
+++ project/shop/views.py
@@ -5,8 +5,20 @@ from .models import Product, Category
 def home(request):
     products = Product.objects.all()
     categories = Category.objects.all()
+    product_name = request.GET.get("search")
     category_name = request.GET.get("category")
     filter_name = request.GET.get("filter")
+    min_price = request.GET.get("min_price")
+    max_price = request.GET.get("max_price")
+
+    if min_price:
+        products = products.filter(price__gte=min_price)
+
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    if product_name:
+        products = products.filter(name__icontains=product_name)
 
     if category_name:
         category = Category.objects.get(name=category_name)
@@ -15,12 +27,16 @@ def home(request):
     match filter_name:
         case "price_increase":
             products = products.order_by("price")
+
         case "price_decrease":
             products = products.order_by("-price")
+
         case "rating_increase":
             products = products.order_by("rating")
+
         case "rating_decrease":
             products = products.order_by("-rating")
+
     return render(
         request, "shop/index.html", {"products": products, "categories": categories}
     )
@@ -32,4 +48,4 @@ def about(request):
 
 def product_details(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    return render(request, "shop/product_details.html", {"products": product})
+    return render(request, "shop/product_details.html", {"product": product})
