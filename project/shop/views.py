afe2369a9187d1db15999f034d94914aedc16dc6
Yar00myr <ivanucaromir@gmail.com>
2025-03-31 21:25:43 +0100

    done homework

--- project/shop/views.py
+++ project/shop/views.py
@@ -10,12 +10,20 @@ def home(request):
     filter_name = request.GET.get("filter")
     min_price = request.GET.get("min_price")
     max_price = request.GET.get("max_price")
+    start_date = request.GET.get("start_date")
+    end_date = request.GET.get("end_date")
 
     if min_price:
         products = products.filter(price__gte=min_price)
 
     if max_price:
         products = products.filter(price__lte=max_price)
+
+    if start_date:
+        products = products.filter(created_at__gte=start_date)
+
+    if end_date:
+        products = products.filter(created_at__lte=end_date)
 
     if product_name:
         products = products.filter(name__icontains=product_name)
@@ -36,9 +44,11 @@ def home(request):
 
         case "rating_decrease":
             products = products.order_by("-rating")
+            
+    length = len(products)
 
     return render(
-        request, "shop/index.html", {"products": products, "categories": categories}
+        request, "shop/index.html", {"products": products, "categories": categories, "length": length}
     )
 
 
