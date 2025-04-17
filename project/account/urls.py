d2891953c30ecca459c2bbaa815c540e470bbbd6
Yar00myr <ivanucaromir@gmail.com>
2025-04-17 16:43:28 +0100

    lesson

--- project/account/urls.py
+++ project/account/urls.py
@@ -36,4 +36,35 @@ urlpatterns = [
     ),
     path("confirm_email/", confirm_email, name="confirm_email"),
     path("confirm_registration", confirm_registration, name="confirm_registration"),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="account/password_reset/form.html",
+            email_template_name="account/password_reset/email.html",
+            success_url=reverse_lazy("account:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="account/password_reset/done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="account/password_reset/confirm.html",
+            success_url=reverse_lazy("account:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="account/password_reset/complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
